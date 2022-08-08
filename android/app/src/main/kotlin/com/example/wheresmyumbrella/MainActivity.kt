5252ca6c0e80bba01878898f01fa1272fe792eac
Vikhyat Gaur <gaurvikhyat@gmail.com>
2022-08-08 14:35:43 +0530

    Weather

---
+++ android/app/src/main/kotlin/com/example/wheresmyumbrella/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wheresmyumbrella
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,45 @@
+import '../services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
+
+const apiKey = 'ebc214cc0529fc3b6147e5474d6fd159';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+    print('this line of code is trigerred');
+  }
+
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
